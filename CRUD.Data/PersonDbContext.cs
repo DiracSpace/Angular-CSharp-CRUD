b7eb752d1c64371a7578d60f4a3beb8adc22894e
DiracSpace <www.ralphlaurenpolo@gmail.com>
2021-02-10 02:15:24 -0600

    Database connection, data model and other classes implemented

---
+++ CRUD.Data/PersonDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CRUD.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Data
+{
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Persons> Person { get; set; }
+    }
+}
