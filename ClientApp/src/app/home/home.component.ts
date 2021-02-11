e87ede8154e3a822c2ef1a18ff1edea0125e2b5b
DiracSpace <www.ralphlaurenpolo@gmail.com>
2021-02-11 01:58:07 -0600

    Started Angular Frontend

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../provider/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  providers: [],
 })
 export class HomeComponent {
+  title = 'Personas';
+  persons = [];
+
+  constructor(private api: ApiService) {
+    this.getAllPeople();
+  }
+
+  getAllPeople = () => {
+    this.api.getAll().subscribe(
+      (data: any) => {
+        this.persons = data;
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    )
+  }
 }
